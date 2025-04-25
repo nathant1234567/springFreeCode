360d58d8fcd366a9fa0a106dddc8b3ec64f106a1
Nathan Thompson <nathanthompson256@outlook.com>
2025-04-25 12:45:15 +0100

    initialize Spring Boot application with basic structure and configuration

---
+++ src/main/java/com/nathan256/storetutorial/StoreTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.nathan256.storetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathan256/storetutorial/StoreTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan256.storetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
