fc246eeabe0fc25ed738a8086b8cef5674e50573
Nathan Thompson <nathanthompson256@outlook.com>
2025-04-26 14:29:56 +0100

    Add Maven Wrapper and Spring Boot application setup

---
+++ src/main/java/com/nathan256/springfreecode/SpringFreeCodeApplication.java
@@ -0,0 +1,13 @@
+package com.nathan256.springfreecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFreeCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFreeCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nathan256/springfreecode/SpringFreeCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan256.springfreecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFreeCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
