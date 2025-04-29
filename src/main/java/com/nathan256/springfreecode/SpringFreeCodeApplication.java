019f2b859dde40c5f235c1a258ad84356b9d4407
Nathan Thompson <nathanthompson256@outlook.com>
2025-04-29 21:40:26 +0100

    Simplify main method by removing unnecessary service calls and default properties setup

--- src/main/java/com/nathan256/springfreecode/SpringFreeCodeApplication.java
+++ src/main/java/com/nathan256/springfreecode/SpringFreeCodeApplication.java
@@ -9,16 +9,7 @@ import java.util.Collections;
 public class SpringFreeCodeApplication {
 
     public static void main(String[] args) {
-        var app = new SpringApplication(SpringFreeCodeApplication.class);
-        app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev")); // settings profile
-        var ctx = app.run(args);
-
-        MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-        System.out.println(myFirstService.tellAStory());
-        System.out.println(myFirstService.getCustomPropertyFromAnotherFile());
-        System.out.println(myFirstService.getCustomPropertyFromAnotherFile2());
-        System.out.println(myFirstService.getCustomProperty());
-        System.out.println(myFirstService.getCustomPropertyInt());
+        SpringApplication.run(SpringFreeCodeApplication.class);
     }
 
 }
